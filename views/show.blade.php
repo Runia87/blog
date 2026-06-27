d41704a25c83617f0026f2ce281bf51b7962206f
Runia87 <ainur.sarybayeva@gmail.com>
2026-06-27 22:21:04 +0500

    Add files via upload

---
+++ views/show.blade.php
@@ -0,0 +1,16 @@
+@extends('layout')
+
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+
+        <img src="/{{$imageInView}}" class="img-thumbnail gallery-image">
+        </div>
+
+    </div>
+
+</div>
+
+@endsection
