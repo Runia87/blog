b01be8748efa6b3e77c6385db74237cfbacb45b8
Runia87 <ainur.sarybayeva@gmail.com>
2026-06-27 22:35:11 +0500

    Create web.php

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\ImagesController;
+use App\Http\Controllers\HomeController;
+ 
+
+
+Route::get('/', [ImagesController::class, 'index']);
+
+
+Route::get('/about',  [HomeController::class, 'about']);
+
+Route::get('/create',[ImagesController::class, 'create'] );
+
+Route::get('/show/{id}',[ImagesController::class, 'show'] );
+
+Route::get('/edit/{id}',[ImagesController::class, 'edit']  );
+
+Route::post('/store', [ImagesController::class, 'store']);
+
+Route::post('/update/{id}', [ImagesController::class, 'update'] );
+
+Route::get('/delete/{id}', [ImagesController::class, 'delete']);
+
